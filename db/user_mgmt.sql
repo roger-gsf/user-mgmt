802ed4c45c48f159d304fdbab6086df7debaa5ef
Róger <rogerferreira610@gmail.com>
2024-09-05 17:48:57 -0300

    Updating

--- db/user_mgmt.sql
+++ db/user_mgmt.sql
@@ -2,8 +2,8 @@ CREATE DATABASE user_mgmt;
 USE user_mgmt;
 
 CREATE TABLE users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_name VARCHAR(100) NOT NULL,
-    user_email VARCHAR(100) NOT NULL UNIQUE,
-    user_password VARCHAR(100) NOT NULL
+    user_id SMALLINT AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(255) NOT NULL,
+    user_email VARCHAR(255) NOT NULL UNIQUE,
+    user_password VARCHAR(255) NOT NULL
 );
